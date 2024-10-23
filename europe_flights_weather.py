a8ba223199f7b0aed9cabc20756cddd0ab65b3c6
dstairlines24 <dstairlinessep24@gmail.com>
2024-10-23 08:14:38 +0000

    maj vols Eeurope

--- europe_flights_weather.py
+++ europe_flights_weather.py
@@ -1,7 +1,5 @@
 from pymongo import MongoClient
-from datetime import datetime
-from api_requests import APIRequests 
-from functions import FlightProcessor, FlightDataError # Importer les classes FlightProcessor et FlightDataError
+from functions import FlightProcessor
 
 # Connexion à MongoDB
 client = MongoClient(host="localhost", port=27017, username="dstairlines", password="dstairlines")
@@ -10,37 +8,31 @@ db = client.app_data
 flightprocessor = FlightProcessor() # Créer une instance de la classe FlightProcessor
 
 def today_europe_flights_append():
-	#Requête API pour récupérer les vols landed (aviationstack) => à exécuter tous les jours
-	flight_info = flightprocessor.get_flights_landed()
+	   # Requête API pour récupérer les vols "landed" (aviationstack)
+    flight_info = flightprocessor.get_flights_landed()
 
-	#Filtrer les vols avec un départ et une arrivée en Europe
-	if flight_info and 'data' in flight_info:
-		europe_flights = [
-		flight for flight in flight_info['data']
-		if flight.get('departure', {}).get('timezone') and flight.get('arrival', {}).get('timezone') and flight['departure']['timezone'].startswith('Europe') and 
-		flight['arrival']['timezone'].startswith('Europe')
-		]
+    # Filtrer les vols avec un départ et une arrivée en Europe
+    if flight_info and 'data' in flight_info:
+        europe_flights = [
+            flight for flight in flight_info['data']
+            if flight.get('departure', {}).get('timezone') and flight.get('arrival', {}).get('timezone') and
+               flight['departure']['timezone'].startswith('Europe') and flight['arrival']['timezone'].startswith('Europe')
+        ]
 
-		if europe_flights:
-			try:
-				europe_flights_processed = flightprocessor.process_flight_AS_list(europe_flights)
-				message = f"{len(europe_flights_processed)} vols complétés avec les infos météo sur le trajet."
-				print(message)
+        if europe_flights:
+            # Traiter la liste des vols et insérer dans la collection MongoDB
+            result = flightprocessor.process_flight_AS_list(europe_flights, db.europe_flights)
 
-				#Ajout des vols du jour à la collection globale
-				result = db.europe_flights.insert_many(europe_flights_processed)
-				if result:
-					print(f"Documents ajoutés : {len(result.inserted_ids)}")
-					return {"Documents ajoutés" : len(result.inserted_ids)}
-				else:
-					return {"error": "Aucun document à ajouter."}, 404
-				
-			except FlightDataError as e:
-				return {"error": e.args[0], "details": e.details}, 400
-		else:
-			return {"error": "Aucun vol trouvé en Europe.", "details": europe_flights}, 404
-	else:
-		return {"error": "Aucun vol trouvé ou problème API AS", "details": flight_info}
+            # Résumé des opérations
+            print(f"{result['vols traités']} vols traités et insérés dans la base de données.")
+            if result["vols échoués"]:
+                print(f"{result['vols échoués']} vols ont échoué et n'ont pas été insérés.")
+
+            return result
+        else:
+            return {"error": "Aucun vol trouvé en Europe.", "details": europe_flights}, 404
+    else:
+        return {"error": "Aucun vol trouvé ou problème API AS", "details": flight_info}
 
 
 today_europe_flights_append()
