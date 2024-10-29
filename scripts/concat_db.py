ecbad1a532f4f1432904c326d218b9427e32cd0e
guillaume <guillaume.gmn@gmail.com>
2024-10-29 05:21:41 +0100

    modif concat_db.py

--- scripts/concat_db.py
+++ scripts/concat_db.py
@@ -14,11 +14,33 @@ for collection_name in collections:
     if collection_name in db.list_collection_names():  # Vérifier si la collection existe
         collection = db[collection_name]
         data.extend(list(collection.find({}, {"_id": 0})))  # Exclure le champ '_id'
+        # Vérifier le contenu de chaque collection ajoutée
+        # print(f"Données récupérées de la collection '{collection_name}':")
+        # print(list(collection.find({}, {"_id": 0})))
     else:
         print(f"La collection '{collection_name}' n'existe pas dans la base de données.")  # Message d'erreur
 
 # Création du DataFrame pandas MongoDB
 df_new = pd.DataFrame(data)
+
+# Création de colonnes temporaires pour récupérer les attributs 
+df_new['dep_iata'] = df_new['departure'].apply(lambda x: x.get('iata') if isinstance(x, dict) else None)
+df_new['arr_iata'] = df_new['arrival'].apply(lambda x: x.get('iata') if isinstance(x, dict) else None)
+df_new['departure_scheduled'] = df_new['departure'].apply(lambda x: x.get('scheduled') if isinstance(x, dict) else None)
+#Cette colonne servira à tester les doublons
+df_new['dep_arr_scheduled'] = df_new.apply(
+    lambda row: f"{row['dep_iata']}_{row['arr_iata']}_{row['departure_scheduled']}", axis=1
+)
+
+# print("Données récupérées dans df_new:")
+# print(df_new)
+print(f"\n\nNombre d'enregistrements dans les collections (df_new): {len(df_new)}")
+
+# Affichage du nombre de doublons dans 'dep_arr_scheduled'
+dup_count = df_new['dep_arr_scheduled'].value_counts()
+dup_count = dup_count[dup_count > 1]  # Filtrer pour n'afficher que les valeurs avec des doublons
+print("Nombre de doublons dans 'df_new' :")
+print(dup_count)
 
 # Récupération de la collection 'final_flights' existante s'il y a des enregistrements
 if db.final_flights.estimated_document_count() > 0:
@@ -28,21 +50,55 @@ if db.final_flights.estimated_document_count() > 0:
     df_existing = df_existing.drop(columns=['_id'], errors='ignore')
 
     # Création de colonnes temporaires pour récupérer les attributs 
-    df_existing['flight_iata'] = df_existing['flight'].apply(lambda x: x.get('iata') if isinstance(x, dict) else None)
+    df_existing['dep_iata'] = df_existing['departure'].apply(lambda x: x.get('iata') if isinstance(x, dict) else None)
+    df_existing['arr_iata'] = df_existing['arrival'].apply(lambda x: x.get('iata') if isinstance(x, dict) else None)
     df_existing['departure_scheduled'] = df_existing['departure'].apply(lambda x: x.get('scheduled') if isinstance(x, dict) else None)
     
-    # Concaténation et suppression des doublons basés sur 'flight_iata' et 'departure_scheduled'
-    df_combined = pd.concat([df_existing, df_new]).drop_duplicates(subset=['flight_iata', 'departure_scheduled'])
+    #Cette colonne servira à tester les doublons
+    df_existing['dep_arr_scheduled'] = df_existing.apply(
+        lambda row: f"{row['dep_iata']}_{row['arr_iata']}_{row['departure_scheduled']}", axis=1
+    )
+
+    # Vérification des données dans df_existing avant la concaténation
+    # print("Données existantes dans final_flights:")
+    # print(df_existing)
+    print(f"\n\nNombre d'enregistrements existants dans final_flights (df_existing): {len(df_existing)}")
+
+    # Affichage du nombre de doublons dans 'dep_arr_scheduled'
+    dup_count2 = df_existing['dep_arr_scheduled'].value_counts()
+    dup_count2 = dup_count2[dup_count2 > 1]  # Filtrer pour n'afficher que les valeurs avec des doublons
+    print("Nombre de doublons dans 'df_existing' :")
+    print(dup_count2)
+
+    # Concaténation des deux DataFrames
+    df_combined = pd.concat([df_existing, df_new])
+    print(f"\n\nNombre d'enregistrements après concat (df_combined): {len(df_combined)}")
+
+    # Affichage du nombre de doublons dans 'dep_arr_scheduled' dans df_combined avant suppression des doublons
+    dup_count_combined = df_combined['dep_arr_scheduled'].value_counts()
+    dup_count_combined = dup_count_combined[dup_count_combined > 1]  # Filtrer pour n'afficher que les valeurs avec des doublons
+    print("Nombre de doublons dans 'df_combined' avant suppression :")
+    print(dup_count_combined)
+
+    # Suppression des doublons basés sur 'dep_arr_scheduled'
+    df_combined = df_combined.drop_duplicates(subset=['dep_arr_scheduled'])
 else:
     # Si 'final_flights' est vide, utiliser seulement les nouvelles données
     df_combined = df_new
 
-# Suppression des colonnes temporaires avant insertion
-df_combined = df_combined.drop(columns=['flight_iata', 'departure_scheduled'], errors='ignore')
+# Vérification des données dans df_combined après suppression des doublons
+# print("Données combinées après suppression des doublons:")
+# print(df_combined)
+print(f"\n\nNombre d'enregistrements après concat et suppression des doublons (df_combined): {len(df_combined)}")
 
+
+# Suppression des colonnes temporaires avant insertion
+df_combined = df_combined.drop(columns=['dep_arr_scheduled'], errors='ignore')
 
 # Vider la collection 'final_flights' et insérer les données combinées sans doublons
 db.final_flights.delete_many({})  # Supprimer tous les documents existants dans 'final_flights'
 db.final_flights.insert_many(df_combined.to_dict('records'))  # Insérer les données sans les IDs
 
+# Vérification finale
+print(f"\nNombre d'enregistrements dans final_flights après insertion: {db.final_flights.estimated_document_count()}")
 print("Données combinées et insérées dans 'final_flights' avec succès.")
