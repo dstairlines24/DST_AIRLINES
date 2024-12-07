dde5dd8c1d5bec9273200d1b08b571ce2d975699
guillaume <guillaume.gmn@gmail.com>
2024-12-07 16:40:16 +0100

    debbog cron

--- app_admin/start.sh
+++ app_admin/start.sh
@@ -7,10 +7,10 @@ python init_db.py
 jupyter notebook --no-browser --allow-root --NotebookApp.token='' &
 
 # CRON JOB
-chmod +x ~/cronpipe.sh
+chmod +x cronpipe.sh
 # Ajouter le cron job dans le crontab de l'utilisateur
 # Cela vérifie d'abord si la tâche existe déjà pour éviter les doublons
-(crontab -l 2>/dev/null | grep -v "~/cronpipe.sh"; echo "0 8 * * * ~/cronpipe.sh") | crontab -
+(crontab -l 2>/dev/null | grep -v "cronpipe.sh"; echo "0 8 * * * cronpipe.sh") | crontab -
 
 # Lancer cron en arrière-plan
 service cron start
